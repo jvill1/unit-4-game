850406f06b73c9f2e2bf0a51cb3d10cb83e6ce81
John Villani <jvill1@me.com>
2019-08-29 19:44:03 -0400

    html, js start, js psuedocode

---
+++ Untitled-1.js
@@ -0,0 +1,32 @@
+var crystal1 = math[1];
+var crystal2 = math[2];
+var crystal3 = math[3];
+var crystal4 = math[4];
+
+var targetNumber;
+
+var userCounter = "";
+
+var win;
+var lose;
+
+var array=
+
+function math()=
+    return math.random()
+};
+
+for (var i=0;i<array.length;i++){
+    var crystal = $("<div>")
+    crystal.addClass("crystal")
+    charImg.attr("src", "assests/images/" + characters[i] + jpg)
+}
+
+function myFunction (){}
+
+var game = {
+    state;
+    playstate: funtion () (alkdjf
+        ;
+    )
+}
